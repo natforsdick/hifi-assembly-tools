9a484a969a4be7ae1ee9e79b5c87dfccfc250875
natforsdick <natalieforsdick@gmail.com>
2021-08-25 17:05:01 +1200

    Modified for asm2-p

--- purge_dups/02-purge-haplotigs-overlaps.sh
+++ purge_dups/02-purge-haplotigs-overlaps.sh
@@ -11,16 +11,17 @@
 ##########
 # PARAMS
 ##########
-INDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm1-Canu/
-OUTDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm1-Canu/purge_dups/
+INDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm2-hifiasm-p/
+OUTDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm2-hifiasm-p/purge_dups/
 PURGE_DUPS=/nesi/nobackup/ga03186/purge_dups/bin/
+PRI=asm2-hifiasm-p.p_ctg
 ##########
 
 cd $OUTDIR
 
 # Step 02
 ${PURGE_DUPS}purge_dups -2 -T cutoffs -c PB.base.cov \
-${OUTDIR}kaki-asm.contigs.split.self.paf.gz > dups.bed 2> purge_dups.log
+${OUTDIR}${PRI}.split.self.paf.gz > dups.bed 2> purge_dups.log
 
 # Step 03
-${PURGE_DUPS}get_seqs -e dups.bed ${INDIR}kaki-asm.contigs.fasta 
+${PURGE_DUPS}get_seqs -e dups.bed ${INDIR}${PRI}.fa 
