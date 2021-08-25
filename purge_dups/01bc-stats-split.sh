1bbbd6687b0e6107bba2e92a9d4c316b205cf2c2
natforsdick <natalieforsdick@gmail.com>
2021-08-25 16:58:34 +1200

    Modified for asm2-p

--- purge_dups/01bc-stats-split.sh
+++ purge_dups/01bc-stats-split.sh
@@ -9,15 +9,16 @@
 # Split the assembly.
 # Takes a negligible amount of resource, so can be run without SLURM wrapper.
 
-INDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm1-Canu/
-OUTDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm1-Canu/purge_dups
+INDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm2-hifiasm-p/
+OUTDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm2-hifiasm-p/purge_dups/
+PRI=asm2-hifiasm-p.p_ctg
 PURGE_DUPS=/nesi/nobackup/ga03186/purge_dups/bin/
 
 cd $OUTDIR
 
 # Produce PB.base.cov and PB.stat files
-${PURGE_DUPS}pbcstat ${OUTDIR}kaki-hifi-to-asm1-Canu.paf.gz
+${PURGE_DUPS}pbcstat ${OUTDIR}${PRI}-mapped.paf.gz
 
 ${PURGE_DUPS}calcuts ${OUTDIR}PB.stat > cutoffs 2> ${OUTDIR}calcults.log
 
-${PURGE_DUPS}split_fa ${INDIR}kaki-asm.contigs.fasta > ${OUTDIR}kaki-asm.contigs.split
+${PURGE_DUPS}split_fa ${INDIR}${PRI}.fa > ${OUTDIR}${PRI}.split
