3a037293b53e593cfc2615ec1b8c9c483c08a66d
natforsdick <natalieforsdick@gmail.com>
2021-08-27 15:39:43 +1200

    Modified to ifelse

--- purge_dups/04-PD-purge.sh
+++ purge_dups/04-PD-purge.sh
@@ -7,6 +7,7 @@
 # Purge_dups pipeline
 # Purge haplotigs and overlaps
 # Get purged primary and haplotig sequences from draft assembly
+# Takes two arguments: PRI/ALT and R1/R2
 
 ##########
 # PARAMS
@@ -14,7 +15,7 @@
 INDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm2-hifiasm-p/
 OUTDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm2-hifiasm-p/purge_dups/
 PURGE_DUPS=/nesi/nobackup/ga03186/purge_dups/bin/
-PRE=asm2-hifiasm-p- # PREFIX
+PRE=asm2-hifiasm-p # PREFIX
 PRI=p_ctg
 ALT=a_ctg
 R1=01P-
@@ -23,20 +24,34 @@ R2=02P- # Designate cutoffs round - either default (01) or modified (02) and whe
 
 cd $OUTDIR
 
-# Step 04a: Purge haplotigs and overlaps
 # R1 version
-${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}${ALT}-cutoffs -c ${R1}${PRE}${ALT}-PB.base.cov \
-${R1}${PRE}${ALT}.split.self.paf.gz > ${R1}${PRE}${ALT}-dups.bed 2> ${R1}${PRE}${ALT}-purge_dups.log
+if [ "$1" == "PRI" ]; then
+  if [ "$2" == "R1" ]; then
+    # Step 04a: Purge haplotigs and overlaps
+    ${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}-${PRI}-cutoffs -c ${R1}${PRE}-${PRI}-PB.base.cov \
+    ${R1}${PRE}-${PRI}.split.self.paf.gz > ${R1}${PRE}-${PRI}-dups.bed 2> ${R1}${PRE}-${PRI}-purge_dups.log
 
-# R2 version
-#${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}${ALT}-cutoffs -c ${R1}${PRE}${ALT}-PB.base.cov \
-#${R1}${PRE}${ALT}.split.self.paf.gz > ${R2}${PRE}${ALT}-dups.bed 2> ${R2}${PRE}${ALT}-purge_dups.log
+    # Step 04b: Get purged primary and haplotig sequences from draft assembly
+    ${PURGE_DUPS}get_seqs -e ${R1}${PRE}-${PRI}-dups.bed ${INDIR}${PRE}.${PRI}.fa
+  
+  elif [ "$2" == "R2" ]; then
+    ${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}-${PRI}-cutoffs -c ${R1}${PRE}-${PRI}-PB.base.cov \
+    ${R1}${PRE}-${PRI}.split.self.paf.gz > ${R2}${PRE}-${PRI}-dups.bed 2> ${R2}${PRE}-${PRI}-purge_dups.log
 
+    ${PURGE_DUPS}get_seqs -e ${R2}${PRE}-${PRI}-dups.bed ${INDIR}${PRE}.${PRI}.fa
+  fi
 
-# Step 04b: Get purged primary and haplotig sequences from draft assembly
-# R1 version:
-${PURGE_DUPS}get_seqs -e ${R1}${PRE}${ALT}-dups.bed ${INDIR}${PRE}.${ALT}.fa
+elif [ "$1" == "ALT" ]; then
+  if [ "$2" == "R1" ]; then
+    ${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}-${ALT}-cutoffs -c ${R1}${PRE}-${ALT}-PB.base.cov \
+    ${R1}${PRE}-${ALT}.split.self.paf.gz > ${R1}${PRE}-${ALT}-dups.bed 2> ${R1}${PRE}-${ALT}-purge_dups.log
 
-# R2 version
-#${PURGE_DUPS}get_seqs -e ${R2}${PRE}${ALT}-dups.bed ${INDIR}${PRE}.${PRI}.fa
+    ${PURGE_DUPS}get_seqs -e ${R1}${ALT}-${PRI}-dups.bed ${INDIR}${R1}${PRE}.${ALT}.hap-merged.fa
 
+  elif [ "$2" == "R2" ]; then
+    ${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}${ALT}-cutoffs -c ${R1}${PRE}${ALT}-PB.base.cov \
+    ${R1}${PRE}${ALT}.split.self.paf.gz > ${R2}${PRE}${ALT}-dups.bed 2> ${R2}${PRE}${ALT}-purge_dups.log
+
+    ${PURGE_DUPS}get_seqs -e ${R2}${PRE}${ALT}-dups.bed ${INDIR}${R2}${PRE}.${ALT}.hap-merged.fa
+  fi
+fi
