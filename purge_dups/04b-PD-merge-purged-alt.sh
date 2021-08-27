e07707b1d6c00a9058ef34be105ed8d293cb07e4
natforsdick <natalieforsdick@gmail.com>
2021-08-27 17:46:11 +1200

    Added elif

--- purge_dups/04b-PD-merge-purged-alt.sh
+++ purge_dups/04b-PD-merge-purged-alt.sh
@@ -12,7 +12,7 @@
 INDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm3-hic-hifiasm-p/
 OUTDIR=/nesi/nobackup/ga03186/kaki-hifi-asm/asm3-hic-hifiasm-p/purge_dups/
 PURGE_DUPS=/nesi/nobackup/ga03186/purge_dups/bin/
-PRE=asm3-hic-hifiasm-p- # PREFIX
+PRE=asm3-hic-hifiasm-p # PREFIX
 PRI=p_ctg
 ALT=a_ctg
 R1=01P- # Designate cutoffs round - either default (01) or modified (02) and whether Primary or Alternate assembly
@@ -22,8 +22,19 @@ R2=02P-
 cd $OUTDIR
 
 # Rename outputs
-mv purged.fa ${R2}${PRE}${ALT}-purged.fa
-mv hap.fa ${R2}${PRE}${ALT}-hap.fa
+if [ "$1" == "PRI" ]; then
+
+mv purged.fa ${R1}${PRE}${PRI}-purged.fa
+mv hap.fa ${R1}${PRE}${PRI}-hap.fa
 
 # Step 05: Merge purged haplotigs (hap.fa) with the alternate draft assembly
-cat ${R2}${PRE}${ALT}-hap.fa ${INDIR}${PRE}.${ALT}.fa > ${INDIR}${R2}${PRE}.${ALT}.hap-merged.fa
+cat ${R1}${PRE}-${PRI}-hap.fa ${INDIR}${PRE}.${PRI}.fa > ${INDIR}${R1}${PRE}.${PRI}.hap-merged.fa
+
+elif [ "$1" == "ALT" ]; then
+
+mv purged.fa ${R1}${PRE}${ALT}-purged.fa
+mv hap.fa ${R1}${PRE}${ALT}-hap.fa
+
+# Step 05: Merge purged haplotigs (hap.fa) with the alternate draft assembly
+cat ${R1}${PRE}-${ALT}-hap.fa ${INDIR}${PRE}.${ALT}.fa > ${INDIR}${R1}${PRE}.${ALT}.hap-merged.fa
+fi
