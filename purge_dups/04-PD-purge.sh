fd64380728db1ce8213b7f6dc2bc6435b0861ef7
natforsdick <natalieforsdick@gmail.com>
2021-08-27 16:08:37 +1200

    Removed EOL backslashes

--- purge_dups/04-PD-purge.sh
+++ purge_dups/04-PD-purge.sh
@@ -28,29 +28,25 @@ cd $OUTDIR
 if [ "$1" == "PRI" ]; then
   if [ "$2" == "R1" ]; then
     # Step 04a: Purge haplotigs and overlaps
-    ${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}-${PRI}-cutoffs -c ${R1}${PRE}-${PRI}-PB.base.cov \
-    ${R1}${PRE}-${PRI}.split.self.paf.gz > ${R1}${PRE}-${PRI}-dups.bed 2> ${R1}${PRE}-${PRI}-purge_dups.log
+    ${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}-${PRI}-cutoffs -c ${R1}${PRE}-${PRI}-PB.base.cov ${R1}${PRE}-${PRI}.split.self.paf.gz > ${R1}${PRE}-${PRI}-dups.bed 2> ${R1}${PRE}-${PRI}-purge_dups.log
 
     # Step 04b: Get purged primary and haplotig sequences from draft assembly
     ${PURGE_DUPS}get_seqs -e ${R1}${PRE}-${PRI}-dups.bed ${INDIR}${PRE}.${PRI}.fa
   
   elif [ "$2" == "R2" ]; then
-    ${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}-${PRI}-cutoffs -c ${R1}${PRE}-${PRI}-PB.base.cov \
-    ${R1}${PRE}-${PRI}.split.self.paf.gz > ${R2}${PRE}-${PRI}-dups.bed 2> ${R2}${PRE}-${PRI}-purge_dups.log
+    ${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}-${PRI}-cutoffs -c ${R1}${PRE}-${PRI}-PB.base.cov ${R1}${PRE}-${PRI}.split.self.paf.gz > ${R2}${PRE}-${PRI}-dups.bed 2> ${R2}${PRE}-${PRI}-purge_dups.log
 
     ${PURGE_DUPS}get_seqs -e ${R2}${PRE}-${PRI}-dups.bed ${INDIR}${PRE}.${PRI}.fa
   fi
 
 elif [ "$1" == "ALT" ]; then
   if [ "$2" == "R1" ]; then
-    ${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}-${ALT}-cutoffs -c ${R1}${PRE}-${ALT}-PB.base.cov \
-    ${R1}${PRE}-${ALT}.split.self.paf.gz > ${R1}${PRE}-${ALT}-dups.bed 2> ${R1}${PRE}-${ALT}-purge_dups.log
+    ${PURGE_DUPS}purge_dups -2 -T ${R1}${PRE}-${ALT}-cutoffs -c ${R1}${PRE}-${ALT}-PB.base.cov ${R1}${PRE}-${ALT}.split.self.paf.gz > ${R1}${PRE}-${ALT}-dups.bed 2> ${R1}${PRE}-${ALT}-purge_dups.log
 
     ${PURGE_DUPS}get_seqs -e ${R1}${ALT}-${PRI}-dups.bed ${INDIR}${R1}${PRE}.${ALT}.hap-merged.fa
 
   elif [ "$2" == "R2" ]; then
-    ${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}${ALT}-cutoffs -c ${R1}${PRE}${ALT}-PB.base.cov \
-    ${R1}${PRE}${ALT}.split.self.paf.gz > ${R2}${PRE}${ALT}-dups.bed 2> ${R2}${PRE}${ALT}-purge_dups.log
+    ${PURGE_DUPS}purge_dups -2 -T ${R2}${PRE}${ALT}-cutoffs -c ${R1}${PRE}${ALT}-PB.base.cov ${R1}${PRE}${ALT}.split.self.paf.gz > ${R2}${PRE}${ALT}-dups.bed 2> ${R2}${PRE}${ALT}-purge_dups.log
 
     ${PURGE_DUPS}get_seqs -e ${R2}${PRE}${ALT}-dups.bed ${INDIR}${R2}${PRE}.${ALT}.hap-merged.fa
   fi
