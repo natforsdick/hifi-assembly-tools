7f38c94f4818b8698faf0b18a9a94051981e65fc
natforsdick <natalieforsdick@gmail.com>
2022-05-06 14:50:33 +1200

    Reduced resources, added extra explanations

--- 06-merqury/run-merqury.sh
+++ 06-merqury/run-merqury.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -A ga03186
 #SBATCH -J merqury
-#SBATCH --mem=6G
-#SBATCH -c 26
+#SBATCH --mem=5G
+#SBATCH -c 28
 #SBATCH --time=00:15:00
 #SBATCH --output=%x.%j.out
 #SBATCH --error=%x.%j.err
@@ -11,7 +11,7 @@
 
 # run-merqury.sl
 # N Forsdick, 2022-04-05
-# Takes 3 params: $1 = asmdir, $2 = primary asm, $3 = alternate assembly
+# Takes 3 params: $1 = asmdir (must include final /), $2 = primary asm, $3 = alternate assembly
 
 ##########
 # PARAMS #
@@ -19,8 +19,8 @@
 genome=kaki
 outdir=/nesi/nobackup/ga03186/kaki-hifi-asm/asm-stats/merqury/
 asmdir=$1
-asm1=$2
-asm2=$3
+asm1=$2 # PRI
+asm2=$3 # ALT
 ##########
 
 cd $outdir
@@ -31,11 +31,13 @@ asmname=${asmname%.*}
 mkdir -p ${asmname}-merqury
 cd ${asmname}-merqury
 
-if [ -e "$asm1" ] && [ -e "$asm2" ]; then
+echo "$1 $2 $3"
+
+if [ -L ${asm1} ]; then
 	echo "${asm1} and ${asm2} exist"
-else
-	ln -s ${asmdir}${asm1} ${asm1}
-    	ln -s ${asmdir}${asm2} ${asm2}
+	else
+		ln -s ${asmdir}${asm1} ${asm1}
+    		ln -s ${asmdir}${asm2} ${asm2}
 fi
 
 module purge
